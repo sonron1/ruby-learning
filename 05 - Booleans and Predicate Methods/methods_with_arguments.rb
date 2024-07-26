aed6d866034a2d36834f5e9603c3e0998115ef17
= <=>
2024-07-26 11:18:03 +0100

    Learning about the method in Ruby with one or many arguments.

--- 05 - Booleans and Predicate Methods/methods_with_arguments.rb
+++ 05 - Booleans and Predicate Methods/methods_with_arguments.rb
@@ -5,3 +5,10 @@ text = "Big Mac for Me !"
 puts text.include?("ac")
 
 puts text.include?("Me")
+
+number = [35,25,15,30]
+
+puts number.include?(30)
+puts number.include?(100)
+
+puts("Eat Rice", "and Chicken")
---
+++ 05 - Booleans and Predicate Methods/methods_with_multiple_arguments.rb
@@ -0,0 +1,8 @@
+# Method with mutipl argument
+
+puts 20.between?(19,21)
+puts 10.between?(5,9)
+
+puts
+
+puts (-1.9).between?(-1.9, 1)
