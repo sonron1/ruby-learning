4dd09f3d80cd93b8a9f689f157a41492284a0688
= <=>
2024-07-29 09:09:58 +0100

    Return value, implicite return on the method in ruby.

---
+++ 06 - Methods/return_values_i.rb
@@ -0,0 +1,14 @@
+# Return value
+
+all_caps_firetruck = "firetruck".upcase
+puts all_caps_firetruck
+
+def addition_of_two_numbers(number1, number2)
+  puts "I'm solving your math problem"
+  return number1 + number2
+end
+
+result = addition_of_two_numbers(1, 5)
+
+p result
+
---
+++ 06 - Methods/return_values_ii_implicit_return_values.rb
@@ -0,0 +1,13 @@
+
+def nothing
+  puts "what will be the reutrn value here ?" #puts reutrn Nil
+end
+
+def add_two_numbers(num1, num2)
+  num1 + num2
+end
+
+p add_two_numbers(1, 3)
+
+p nothing
+
